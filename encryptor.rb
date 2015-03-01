decb8cabf561efbeee83b4210293464509872748
Alex Fox <alexdysonfox@gmail.com>
2015-03-01 16:13:36 -0500

    Add rotation logic to encrypt/decrypt methods.

--- encryptor.rb
+++ encryptor.rb
@@ -5,24 +5,24 @@ class Encryptor
     Hash[characters.zip(rotated_characters)]
   end
 
-  def encrypt_letter(letter)
-    rotation = 13
+  def encrypt_letter(letter, rotation)
     rotating_cipher = cipher(rotation)
     rotating_cipher[letter]
   end
 
-  def encrypt(string)
+  def encrypt(string, rotation)
     letters = string.split("")
     results = letters.collect do |letter|
-      encrypted_letter = encrypt_letter(letter)
+      encrypted_letter = encrypt_letter(letter, rotation)
     end
     results.join
   end
 
-  def decrypt(string)
+  def decrypt(string, rotation)
     letters = string.split("")
     results = letters.collect do |letter|
-      decrypted_letter = cipher[letter]
+      decrypted_letter = cipher(-rotation)
+      decrypted_letter[letter]
     end
     results.join
   end

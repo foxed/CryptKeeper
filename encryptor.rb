295a4c6494a626618c727f7743de30bc8c903f1c
Alex Fox <alexdysonfox@gmail.com>
2015-03-01 15:45:31 -0500

    Basic custom cipher, encryption/decryption.

---
+++ encryptor.rb
@@ -0,0 +1,32 @@
+class Encryptor
+  def cipher
+    {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
+     'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
+     'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
+     'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
+     'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
+     'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
+     'y' => 'l', 'z' => 'm'}
+  end
+
+  def encrypt_letter(letter)
+    lowercase_letter = letter.downcase
+    cipher[lowercase_letter]
+  end
+
+  def encrypt(string)
+    letters = string.split("")
+    results = letters.collect do |letter|
+      encrypted_letter = encrypt_letter(letter)
+    end
+    results.join
+  end
+
+  def decrypt(string)
+    letters = string.split("")
+    results = letters.collect do |letter|
+      decrypted_letter = cipher[letter]
+    end
+    results.join
+  end
+end
